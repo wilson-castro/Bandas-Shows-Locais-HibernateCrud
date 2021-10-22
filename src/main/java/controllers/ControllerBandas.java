78ae40741f95f3cf78fdec9ab24779e3aed8bd7a
wilson-castro <wilsoncastro.pv@gmail.com>
2021-10-22 19:46:10 -0300

    Criação das estrutura base do projeto

---
+++ src/main/java/controllers/ControllerBandas.java
@@ -0,0 +1,125 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.daos.BandaDao;
+
+@WebServlet(urlPatterns = { "/ControllerBandas", "/bandas", "/bandas/insert", "/bandas/select", "/bandas/update",
+		"/bandas/delete" })
+public class ControllerBandas extends HttpServlet {
+	BandaDao dao = new BandaDao();
+
+	public ControllerBandas() {
+		super();
+
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String action = request.getServletPath();
+
+		if (action.equals("/bandas")) {
+			bandas(request, response);
+		} else if (action.equals("/bandas/insert")) {
+			novaBanda(request, response);
+		} else if (action.equals("/bandas/update")) {
+			editarBanda(request, response);
+		} else if (action.equals("/bandas/delete")) {
+			removerBanda(request, response);
+		} else if (action.equals("/bandas/pesquisar")) {
+			System.out.println("pesquisar");
+		} else {
+			response.sendRedirect("index.jsp");
+		}
+	}
+
+	protected void bandas(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		RequestDispatcher rd = request.getRequestDispatcher("/ListarBandas");
+		rd.forward(request, response);
+
+	}
+
+	protected void editarBanda(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		/*Banda banda = new Banda();
+
+		int idBanda = Integer.parseInt(request.getParameter("idBanda"));
+		String nome = request.getParameter("nome");
+		String generoString = request.getParameter("selectGeneros");
+		Genero genero = Genero.valueOf(generoString);
+
+		banda.setIdBanda(idBanda);
+		banda.setGenero(genero);
+		banda.setNome(nome);*/
+
+		if (request.getParameterValues("List_ShowsIDs") == null) {
+		//	dao.alterarBanda(banda, null);
+
+		} else {
+			/*String[] checkboxIdsList = request.getParameterValues("List_ShowsIDs");
+			int size = checkboxIdsList.length;
+
+			int[] idsList = new int[size];
+
+			for (int i = 0; i < size; i++) {
+				idsList[i] = Integer.parseInt(checkboxIdsList[i]);
+			}
+			dao.alterarBanda(banda, idsList);*/
+		}
+
+		response.sendRedirect("/projeto/bandas");
+	}
+
+	protected void novaBanda(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	/*	Banda banda = new Banda();
+
+		String nome = request.getParameter("nome");
+		String generoString = request.getParameter("selectGeneros");
+		Genero genero = Genero.valueOf(generoString);
+
+		banda.setGenero(genero);
+		banda.setNome(nome);*/
+
+		if (request.getParameterValues("List_ShowsIDs") == null) {
+		//	dao.adicionarBanda(banda, null);
+
+		} else {
+		/*	String[] checkboxIdsList = request.getParameterValues("List_ShowsIDs");
+			int size = checkboxIdsList.length;
+
+			int[] idsList = new int[size];
+
+			for (int i = 0; i < size; i++) {
+				idsList[i] = Integer.parseInt(checkboxIdsList[i]);
+			}
+			dao.adicionarBanda(banda, idsList);*/
+		}
+
+		response.sendRedirect("/projeto/bandas");
+	}
+
+	protected void removerBanda(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	/*	int idBanda = Integer.parseInt(request.getParameter("idBanda"));
+		Banda banda = new Banda();
+
+		banda.setIdBanda(idBanda);
+
+		dao.deletarBanda(banda);
+
+		response.sendRedirect("/projeto/bandas");*/
+
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,37 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<title>Agenda de contatos</title>
+</head>
+<body>
+
+	<div class="app">
+		<h1>Projeto CRUD</h1>
+
+		<div class="cards-group">
+			<div class="card" id="bandas">
+					<h4>
+						<b>Bandas</b>
+					</h4>
+					<a href="bandas" class="Botao1">Acessar</a>
+			</div>
+			<div class="card" id="shows">
+					<h4>
+						<b>Shows</b>
+					</h4>
+					<a href="shows" class="Botao1">Acessar</a>
+			</div>
+			<div class="card" id="locais">
+					<h4>
+						<b>Locais</b>
+					</h4>
+					<a href="locais" class="Botao1">Acessar</a>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
